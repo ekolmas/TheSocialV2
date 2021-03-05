c21711b7bed86c9c02f4e1979971d825e38d8c2c
ekolmas <66930465+ekolmas@users.noreply.github.com>
2021-03-06 01:08:18 +0800

    added configuration and started on profile controller
    create function to handle submissions and requests and create redirect to profile method to redirect the user to a file in the view folder

---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+ require_once "config.php";
+ require_once "controllers/profileController.php";
+
+//Getting the full url
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $url = "https://";
+} else {
+    $url = "http://";
+}
+
+//Append the hostname to the url
+$url .= $_SERVER['HTTP_HOST'];
+//Append the requested resource location to the URL
+$url .= $_SERVER['REQUEST_URI'];
+$query = parse_url($url, PHP_URL_QUERY);
+if (strpos($query, "&i=") !== false) {
+    $query = mb_substr(parse_url($url, PHP_URL_QUERY), 0, -4);
+} else {
+    $query = parse_url($url, PHP_URL_QUERY);
+}
+
+redirectToProfile($query);
+
+?>
